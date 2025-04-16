70ecf204c55f8c06d8ee2a814f1a004a6174305d
Alberto Felipe <felipe0032sena@gmail.com>
2025-04-16 11:27:32 -0300

    refactor: implement custom validation messages to form requests

--- app/Http/Requests/UpdateToolRequest.php
+++ app/Http/Requests/UpdateToolRequest.php
@@ -33,4 +33,20 @@ class UpdateToolRequest extends FormRequest
             'tags.*' => ['sometimes', 'string', 'distinct'],
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'The title is required.',
+            'title.string' => 'The title must be a string.',
+            'title.max' => 'The title may not be greater than 255 characters.',
+            'description.required' => 'The description is required.',
+            'description.string' => 'The description must be a string.',
+            'link.required' => 'The link is required.',
+            'link.url' => 'The link must be a valid URL.',
+            'tags.array' => 'The tags must be an array.',
+            'tags.*.string' => 'Each tag must be a string.',
+            'tags.*.distinct' => 'Duplicate tags are not allowed.',
+        ];
+    }
 }
