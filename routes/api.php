58700761219b336a60068556f2234ead79d15e44
albertofelipe <felipe0032sena@gmail.com>
2025-04-15 22:47:32 -0300

    chore: setup project for JWT auth (User model, routes, config, migration, composer)

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ToolController;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tools', ToolController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::apiResource('tools', ToolController::class)->only(['index']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('tools', ToolController::class)->only(['update', 'destroy', 'store', 'show']);	
+});
