f4523ec9973b29ca75b2d62c9c4967a0b2d18d00
Alberto Felipe <felipe0032sena@gmail.com>
2025-04-17 14:32:44 -0300

    docs: set up documentation with L5-Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,25 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="API de Ferramentas",
+ *     version="1.0",
+ *     description="Documentação da API responsável por endpoints relacionados a ferramentas."
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Servidor principal"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
     //
